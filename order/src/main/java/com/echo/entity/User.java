8f288962e5c0dd6bcb44edf4a490cc8e801db6f2
chenqi <1163646727@qq.com>
2020-07-15 19:23:28 +0800

    集成spring data jpa

---
+++ order/src/main/java/com/echo/entity/User.java
@@ -0,0 +1,23 @@
+package com.echo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Table(name = "t_user")
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;//姓名
+
+    @Column
+    private Integer age;//年龄
+
+    @Column
+    private Integer addr;//地址
+}
