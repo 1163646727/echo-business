7cb7e0628d11a46131d2e65b37969b25c6fb0cb8
chenqi <1163646727@qq.com>
2020-07-15 18:43:14 +0800

    整合spring 单元测试

---
+++ order/src/test/java/com/echo/Test_1.java
@@ -0,0 +1,24 @@
+package com.echo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * className: Test_1 <BR>
+ * description: <BR>
+ * remark: <BR>
+ * auther: ChenQi <BR>
+ * date: 2020/7/15 18:35 <BR>
+ * version 1.0 jdk1.8 <BR>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test_1 {
+
+    @Test
+    public void test1(){
+        System.out.println("123");
+    }
+}
