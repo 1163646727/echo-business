6006f9fcf05ceae3efe6ec107ac2ffef96a85b23
ChenQi <1163646727@qq.com>
2020-09-22 17:29:39 +0800

    ①集成mybatis_plus ②设置日志打印的扫包范围和级别

--- serviceA/src/main/java/com/echo/ServerAApp.java
+++ serviceA/src/main/java/com/echo/ServerAApp.java
@@ -1,6 +1,7 @@
 package com.echo;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -18,6 +19,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSwagger2Doc
+@MapperScan(basePackages = {"com.echo.mapper","com.echo.dao"})
 public class ServerAApp {
     public static void main(String[] args) {
         SpringApplication.run (ServerAApp.class,args);
